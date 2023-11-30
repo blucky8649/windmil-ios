221b8ac379bacf7190504bd0657c2c5914db081b
blucky8649 <blucky4511@gmail.com>
2023-12-01 00:16:28 +0900

    Initial Commit

---
+++ windmill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  windmill
+//
+//  Created by 이동연 on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ windmill/windmillApp.swift
@@ -0,0 +1,17 @@
+//
+//  windmillApp.swift
+//  windmill
+//
+//  Created by 이동연 on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct windmillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
