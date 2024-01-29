eb8809c76af6b1d01bb139f0556a2dc1c9f0d42a
Lee Dong-Yeon <blucky4511@gmail.com>
2024-01-30 01:31:11 +0900

    ✨ Model 추가, 알고리즘 > 누적 계좌 목록 추가

--- windmill/WindmillAlgorithm.swift
+++ windmill/WindmillAlgorithm.swift
@@ -7,20 +7,47 @@
 
 import Foundation
 
-func solution(totalWeeks: Int, weeklyIncrease: Int, targetWeek: Int) -> Int {
+func createPreset(
+    presetName: String,
+    totalWeeks: Int,
+    weeklyIncrease: Int,
+    targetWeek: Int
+) -> Preset {
     var dp: [Int] = [] // 주차별 내야할 금액이 담긴 배열
     var acc: [Int] = [] // 주차별 총 누적된 납부 금액
-    (0..<totalWeeks).forEach { week in
+    var accountsByWeek: [[Int]] = [[]]
+    var data = (0..<totalWeeks).map { week in
         if (dp.isEmpty) {
             dp.append(weeklyIncrease)
             acc.insert(dp[week], at: week)
-            return
+            accountsByWeek.insert([weeklyIncrease], at: week)
+            return WeeklyData(
+                week: week + 1,
+                amountAcc: acc[week],
+                accountsAcc: accountsByWeek[week]
+            )
         }
         // 이번주 납부 금액 : 이전 납부액 + 현재 주차 * 증가액
         dp.insert(dp[week-1] + weeklyIncrease * (week + 1), at: week)
+        var prevAccounts = [Int](accountsByWeek[week-1])
+        prevAccounts.append(dp[week])
+        accountsByWeek.insert(prevAccounts, at: week)
         acc.insert(dp[week] + acc[week-1], at: week)
+        
+        return WeeklyData(
+            week: week + 1,
+            amountAcc: acc[week],
+            accountsAcc: accountsByWeek[week]
+        )
     }
     print(acc)
     print(dp)
-    return acc[targetWeek - 1]
+    print(accountsByWeek)
+    
+    return Preset(
+        name: presetName,
+        totalWeeks: totalWeeks,
+        weeklyIncrease: weeklyIncrease,
+        weeklyData: data
+    )
 }
---
+++ windmill/models/WeeklyData.swift
@@ -0,0 +1,8 @@
+//
+//  WeeklyData.swift
+//  windmill
+//
+//  Created by 이동연 on 1/30/24.
+//
+
+import Foundation
