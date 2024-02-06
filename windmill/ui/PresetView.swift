50a57b6abf52f9696a455fa67fc51417b680ae78
Lee Dong-Yeon <83625797+blucky8649@users.noreply.github.com>
2024-02-06 20:55:49 +0900

    ✨ Preset 메인 화면 적용, 테스트 코드 작성 (#11)

--- windmill/ui/PresetView.swift
+++ windmill/ui/PresetView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct PresetView: View {
+    var presetName: String
+    
     var body: some View {
         HStack() {
-            Text("Preset 1").font(.system(size: 14, weight: Font.Weight.medium))
+            Text(presetName).font(.system(size: 14, weight: Font.Weight.medium))
                 .foregroundColor(.onMainContainer)
                 .frame(alignment: .leading)
                 .padding(-9)
@@ -23,7 +25,7 @@ struct PresetView: View {
 
 #Preview {
     VStack {
-        PresetView()
+        PresetView(presetName: "preset 1")
     }
     
 }
--- windmillTests/windmillTests.swift
+++ windmillTests/windmillTests.swift
@@ -9,21 +9,33 @@ import XCTest
 @testable import windmill
 
 final class windmillTests: XCTestCase {
+    
+    var preset: Preset!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        preset = createPreset(presetName: "preset 1", totalWeeks: 26, weeklyIncrease: 5000, targetWeek: 26)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testNameIsEqual() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        print("test!")
+        
+        XCTAssertEqual(preset.name, "preset 1")
+    }
+    
+    func testAccAmounts() throws {
+        let amounts = preset.weeklyData[25].amountAcc
+        XCTAssertEqual(amounts, 16_380_000)
     }
 
     func testPerformanceExample() throws {
