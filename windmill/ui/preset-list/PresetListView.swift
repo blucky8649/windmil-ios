a2667e7ed02dc102dbd95429d21eb8eb94fc986a
Lee Dong-Yeon <blucky4511@gmail.com>
2024-02-14 01:00:01 +0900

    ✨ Preset 리스트 > NavigationStack 추가

--- windmill/ui/preset-list/PresetListView.swift
+++ windmill/ui/preset-list/PresetListView.swift
@@ -12,31 +12,61 @@ struct PresetListView: View {
     let cellDataList: [PresetListCell.Data]
     
     @State var maxHeight: CGFloat = 1000
+    @State private var isShowAddPreset = false
+    @State private var selectedCellIndex: Int? = nil
+    @State private var isNavigationActive: Bool = false
     
     var body: some View {
         GeometryReader { geo in
-            VStack {
-                RoundedRectangle(cornerRadius: 100)
-                    .frame(width: 32, height: 4)
-                    .foregroundColor(.outline)
-                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
-                
-                Text("Presets")
-                    .font(.system(size: 20, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                
-                LazyVStack {
-                    ScrollView {
-                        ForEach(cellDataList, id: \.self) { cell in
-                            PresetListCell(data: cell)
+            NavigationStack {
+                VStack {
+                    HStack {
+                        Text("Presets")
+                            .font(.system(size: 20, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                        Spacer()
+                        Button {
+                            selectedCellIndex = nil
+                            isNavigationActive = true
+                        } label: {
+                            Text("추가")
+                                .foregroundStyle(Color.onSurface)
+                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
                         }
-                    }.frame(width: geo.size.width, height: geo.size.height)
-                }.frame(maxHeight: geo.size.width, alignment: .topLeading)
+                    }
                     
+                    
+                    LazyVStack {
+                        ScrollView {
+                            ForEach(cellDataList.indices, id: \.self) { cell in
+                                let preset = cellDataList[cell]
+                                Button {
+                                    isNavigationActive = preset.isActive
+                                    selectedCellIndex = cell
+                                } label: {
+                                    PresetListCell(data: preset)
+                                }
+                            }
+                            .navigationDestination(isPresented: $isNavigationActive) {
+                                if let index = selectedCellIndex {
+                                    let preset = cellDataList[index]
+                                    AddPresetView(presetName: preset.presetName, paymentWeekPeriod: String(preset.totalWeeks), weeklyIncrease: String(preset.weeklyIncrease))
+                                } else {
+                                    AddPresetView()
+                                }
+                            }
+
+                        }.frame(width: geo.size.width, height: geo.size.height)
+                    }.frame(maxHeight: geo.size.width, alignment: .topLeading)
+                        
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                .background(Color.mainContainer)
+                
             }
-            .frame(maxHeight: .infinity, alignment: .top)
-            .background(Color.mainContainer)
+            
+            
             
         }
         
@@ -55,3 +85,4 @@ struct PresetListView: View {
     }
     
 }
+
